b88ac000ff8eab5bfdd4837875ed9041b8e9094e
axelbro <gusbrobax@student.gu.se>
2022-01-21 15:58:02 +0100

    binary to hexadecimal works for some binary numbers

--- WP1/exercise4/number_conversion.c
+++ WP1/exercise4/number_conversion.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int dec2bin(int dec);
 
+void bin2hex(long int input);
+
 int main() {
+	long int bin = 00001111;
 	system("cls");
-	dec2bin(1200);
+	bin2hex(bin);
 }
 
-//Converts a decimal into binary
+// Converts a decimal into binary
+// https://www.javatpoint.com/c-program-to-convert-decimal-to-binary
 int dec2bin(int dec) {
 	int a[16], i, k, binMod = 0;
 
@@ -26,4 +31,31 @@ int dec2bin(int dec) {
 		printf("%d", a[i]);
 	}
 	return a;
+}
+
+// Converts binary to hexadecimal
+// https://www.tutorialspoint.com/how-to-convert-binary-to-hex-by-using-c-language
+void bin2hex(long int input) {
+	
+	long int binaryval, hexadecimalval = 0, i = 1, remainder;
+	binaryval = input;
+	while (binaryval != 0) {
+		remainder = binaryval % 10;
+		hexadecimalval = hexadecimalval + remainder * i;
+		i = i * 2;
+		binaryval = binaryval / 10;
+	}
+	printf("Equivalent hexadecimal value: %lX", hexadecimalval);
+	return 0;
+	/*long int hexadecimalval = 0, i = 1, remainder;
+	while (binaryval != 0)
+	{
+		remainder = binaryval % 10;
+		hexadecimalval = hexadecimalval + remainder * i;
+		i = i * 2;
+		binaryval = binaryval / 10;
+	}
+	printf("Equivalent hexadecimal value: %lX", hexadecimalval);
+	return hexadecimalval;*/
+
 }
