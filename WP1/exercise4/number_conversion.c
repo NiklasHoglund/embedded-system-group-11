3660c7e0a765e1382d5720af91fdf887469b5e51
Axel Broberg <46676978+AxelBroberg@users.noreply.github.com>
2022-01-22 17:28:58 +0100

    Commented exercise 4

--- WP1/exercise4/number_conversion.c
+++ WP1/exercise4/number_conversion.c
@@ -15,47 +15,37 @@ int main() {
 // Converts a decimal into binary
 // https://www.javatpoint.com/c-program-to-convert-decimal-to-binary
 int dec2bin(int dec) {
-	int a[16], i, k, binMod = 0;
+	int a[16], i, k, binMod = 0; // Initialize and declare variables
 
+	// For loop that runs while dec is larger than 0
 	for (i = 0; dec > 0; i++) {
-		a[i] = dec % 2;
-		dec = dec / 2;
+		a[i] = dec % 2; // Divide the decimal number by 2 with the modulus operator
+		dec = dec / 2; // Divide the decimal number by 2 with the division operator
 	}
 
-	binMod = 8 - (i % 8);
-	for (k = 0; k < binMod; k++) {
-		printf("%d", 0);
+	binMod = 8 - (i % 8); // Get the amount of 0 needed to format the output in an 8-bit format
+	for (k = 0; k < binMod; k++) { // For loop that runs binMod times
+		printf("%d", 0); // Prints 0
 	}
 
-	for (i = i - 1; i >= 0; i--) {
-		printf("%d", a[i]);
+	for (i = i - 1; i >= 0; i--) { // For loop that runs the same amount of times that the first for loop ran but backwards
+		printf("%d", a[i]); // Print number in array at index i
 	}
-	return a;
+	return a; // Return array a
 }
 
 // Converts binary to hexadecimal
 // https://www.tutorialspoint.com/how-to-convert-binary-to-hex-by-using-c-language
 void bin2hex(long int input) {
 	
-	long int binaryval, hexadecimalval = 0, i = 1, remainder;
-	binaryval = input;
-	while (binaryval != 0) {
-		remainder = binaryval % 10;
-		hexadecimalval = hexadecimalval + remainder * i;
-		i = i * 2;
-		binaryval = binaryval / 10;
+	long int binaryval, hexadecimalval = 0, i = 1, remainder; // Initialize and declare variables
+	binaryval = input; //Set variable binaryval to input
+	while (binaryval != 0) { // While the binaryval isn't 0
+		remainder = binaryval % 10; // Set the remainder variable to binaryval divided by 10 using the modulus operator
+		hexadecimalval = hexadecimalval + remainder * i; // To the hexadecimalval variable, add itself plus the remainder * i 
+		i = i * 2; // Double the i variable
+		binaryval = binaryval / 10; // Divide binaryval variable by 10
 	}
 	printf("Equivalent hexadecimal value: %lX", hexadecimalval);
 	return 0;
-	/*long int hexadecimalval = 0, i = 1, remainder;
-	while (binaryval != 0)
-	{
-		remainder = binaryval % 10;
-		hexadecimalval = hexadecimalval + remainder * i;
-		i = i * 2;
-		binaryval = binaryval / 10;
-	}
-	printf("Equivalent hexadecimal value: %lX", hexadecimalval);
-	return hexadecimalval;*/
-
 }
