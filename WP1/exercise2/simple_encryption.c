41bac9daabd2261b1b188257bf454fdb60c61841
axelbro <gusbrobax@student.gu.se>
2022-01-22 15:03:19 +0100

    Commented exercise 2 and changed system(pause) to instead clear the buffer

--- WP1/exercise2/simple_encryption.c
+++ WP1/exercise2/simple_encryption.c
@@ -7,18 +7,29 @@
 #include <stdlib.h>
 
 void main(int argc, char* argv[]) {
+    // Get the value that characters should be "shifted" by in the ASCII-code table
     int encryptValue = atoi(argv[1]);
+
+    // While loop to make the program re-run
     while (1) {
+        // A character array which will hold the word the user wants to encrypt
         char word[25];
         printf("Enter something to encrypt: ");
+        // Scanf_s to let user input their word
         scanf_s("%s", word, 24);
+        // For loop that runs once for every character in the word
         for (int i = 0; i < strlen(word); i++)
         {
+            // Convert letter in word at index i to an integer
             int x = word[i];
+            // Add the encrypt value that the character should be shifted by to the variable x
             x = x + encryptValue;
+            // Replace the letter at index i with the variable x type-casted to a char
             word[i] = (char)x;
         }
+        // Print the encrypted word
         printf("Encrypted word: %s\n", word);
-        system("pause");
+        // Clears the scanf_s buffer so it doesn't use the same value as before
+        while ((getchar()) != '\n');
     }
 }
