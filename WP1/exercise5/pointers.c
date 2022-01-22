5e5af28da00ed68f38733f3c42590e25d54235c3
AhmedYasser-gu <gusyasah@student.gu.se>
2022-01-22 16:27:36 +0100

    Comments for exe 3 and abit in exe 5 , few changes

--- WP1/exercise5/pointers.c
+++ WP1/exercise5/pointers.c
@@ -1,36 +1,44 @@
-// Try to modify and run it and check out
-// the output in the terminal below
-//
+// (C) Ahmed Yasser, Axel Broberg, Niklas Höglund , group: 11 (2022)
+// Work package 1
+// Exercise 5
+// Submission code: XXXXXX (provided by your TA-s)
 
+// Include section
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-// size of array 
+// Defining the size of array 
 #define MAX 10
 
-// largest value in array 
+// Defining the largest value in array 
 #define MAXNUMBER 20
 
-// set a random number in each slot in array 
+// Set a random number in each slot in array 
 void create_random(int* tab);
-// testing purposes 
+
+// Testing purposes 
 void printarray(int* tab, int size);
-// calculate frequency of each number 
+
+// Calculate frequency of each number 
 void count_frequency(int* tab, int* freq);
+
 // Draw a histogram to represent frequency
 void draw_histogram(int* freq);
 
 int main() {
     // array to hold the random numbers
     int* table = (int*)malloc(MAX * sizeof(int));
+
     create_random(table);
     //printarray (table, MAX); // to test only
+    // 
     // create an array and intialize each slot with 0 
     int* frequency = (int*)calloc(MAXNUMBER, sizeof(int));
     count_frequency(table, frequency);
     //printarray(frequency, MAXNUMBER); // to test only
     draw_histogram(frequency);
+
     return 0;
 }
 
