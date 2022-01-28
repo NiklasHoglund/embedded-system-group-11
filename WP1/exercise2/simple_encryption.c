655c6584e4dee28d154ca3cad21fc0b3c4a4ad26
Niklas Hoglund <nikhog@student.chalmers.se>
2022-01-28 14:48:20 +0100

    ex2 with EOF

--- WP1/exercise2/simple_encryption.c
+++ WP1/exercise2/simple_encryption.c
@@ -10,59 +10,59 @@
 
 // Main program section
 
-void main(int argc, char* argv[]) {
+int main(int argc, char* argv[]) {
     // Get the value that characters should be "shifted" by in the ASCII-code table
     int encryptValue = atoi(argv[1]);
     // While loop to make the program re-run
-    while (1) {
+    do {
         // A character array which will hold the word the user wants to encrypt
         char word[25];
         printf("Enter something to encrypt: ");
-        // Scanf_s to let user input their word
-        scanf_s("%s", word, 24);
+
+        fgets(word, sizeof(word), stdin);
         // For loop that runs once for every character in the word
-        for (int i = 0; i < strlen(word); i++)
-        {
+        for (int i = 0; i < strlen(word); i++){
+
             int upper_case = (65 <= word[i] && word[i] <= 90);
             int lower_case = (97 <= word[i] && word[i] <= 122);
             int asciiValue = 0;
+
             // Convert letter in word at index i to an integer
             int x = word[i];
-            printf("\n%d", x);
-            //if the ascii code is upper case
-            if (65 <= word[i] && word[i] <= 90) {
+            //if the letter at index i's integer value is in the span of the upper case letters.
+            if (word[i] = upper_case) {
                 //if the ascii code plus encrypt input from user is higer than highest ascii nr for upper case
                 if (x + encryptValue > 90) {
-                    //reseting count at first upper case letter
+                    //if the encryption goes over the ascii code for Z we return to the start of the alphabet instead of getting other symbols than letters. 
                     x = x + encryptValue - 26;
-                    printf("%d\n", x);
                     asciiValue = x;
                 }
                 else {
-                    // Add the encrypt value that the character should be shifted by to the variable x
+                    // if the ascii value + the encryption value does not go over the ascii code for Z we check what new ascii value it should have. 
                     asciiValue = x + encryptValue;
                 }
             }
-            //if the ascii code is lower case
-            else if (97 <= word[i] && word[i] <= 122) {
+            //if the letter at index i's integer value is in the span of the lower case letters.
+            else if (word[i] = lower_case) {
                 //if the ascii code plus encrypt input from user is higer than highest ascii nr for lower case
                 if (x + encryptValue > 122) {
-                    //reseting count to first lower case letter
+                    /*if the ascii value + the encryption value goes over the ascii code for z we do - 26 on the current ascii value
+                    so that it will always result in a lower case letter and not a symbol from the ascii table.*/
                     x = x + encryptValue - 26;
-                    printf("%d\n", x);
-                    asciiValue = x; 
+                    asciiValue = x;
                 }
                 else {
-                    // Add the encrypt value that the character should be shifted by to the variable x
+                    // if the ascii value + the encryption value does not go over the ascii code for z we check what new ascii value it should have. 
                     asciiValue = x + encryptValue;
                 }
-            } 
+            }
             // Replace the letter at index i with the variable x type-casted to a char
             word[i] = (char)asciiValue;
         }
-        // Print the encrypted word
-        printf("Encrypted word: %s\n", word);
-        // Clears the scanf_s buffer so it doesn't use the same value as before
-        while ((getchar()) != '\n');
-    }
+
+        //If the input != EOF it will print the encrypted word. If the input is EOF it will end the do while loop and terminate the program. 
+        if (!feof(stdin)){ 
+            printf("Encrypted word: %s\n", word);
+        }
+    } while (!feof(stdin));
 }
