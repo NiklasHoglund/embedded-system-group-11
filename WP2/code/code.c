2ee47176df7d0097a6455e6b3e06b2fb235e8ee4
AxelBroberg <axellakris2@gmail.com>
2022-02-02 13:49:44 +0100

    hex fix & create decode

--- WP2/code/code.c
+++ WP2/code/code.c
@@ -8,6 +8,9 @@
 
 int check_values(int nr, int x);
 
+
+void print_byte_as_bits(char val);
+
 int main(int argc, char* argv[])
 {
 	unsigned char byte = 0;
@@ -29,19 +32,20 @@ int main(int argc, char* argv[])
 	else if (!check_values(atoi(argv[5]), 1)) {
 		printf("Argument 5 is out of bounds");
 	}
-	
-	
-	byte = byte + atoi(argv[1]) << 7;
-	byte = byte + atoi(argv[2]) << 4;
-	byte = byte + atoi(argv[3]) << 2;
-	byte = byte + atoi(argv[4]) << 1;
-	byte = byte + atoi(argv[5]);
+	else {
+		byte += atoi(argv[5]);
+		byte += atoi(argv[4]) << 1;
+		byte += atoi(argv[3]) << 2;
+		byte += atoi(argv[2]) << 4;
+		byte += atoi(argv[1]) << 7;
 
-	printf("\nByte: %X\n", (unsigned char)byte);
+		printf("\nByte: %X\n", byte);
+	}
+
 }
 
 int check_values(int nr, int x) {
-	for (int i = x; i > 0; i--)
+	for (int i = x; i > -1; i--)
 	{
 		if (i == nr) {
 			return 1;
@@ -50,22 +54,10 @@ int check_values(int nr, int x) {
 	return 0;
 }
 
-
-
-
-
-/*else if (argv[1] != 0 && argv[1] != 1) {
-		printf("Argument 1 needs to be 1 or 0");
+void print_byte_as_bits(char val)
+{
+	for (int i = 7; 0 <= i; i--)
+	{
+		printf("%c", (val & (1 << i)) ? '1' : '0');
 	}
-	else if (argv[2] != 0 && argv[2] != 1) {
-		printf("Argument 2 needs to be 1 or 0");
-	}
-	else if (argv[3] != 0 && argv[3] != 1 && argv[3] != 2) {
-		printf("Argument 3 needs to be 2, 1 or 0");
-	}
-	else if (argv[4] != 0 && argv[4] != 1 && argv[4] != 2 && argv[4] != 3 && argv[4] != 4) {
-		printf("Argument 4 needs to be 4, 3, 2, 1 or 0");
-	}
-	else if (argv[5] != 0 && argv[5] != 1) {
-		printf("Argument 5 needs to be 1 or 0");
-	}*/
+}
---
+++ WP2/decode/decode.c
@@ -0,0 +1,26 @@
+// (C) Ahmed Yasser, Axel Broberg, Niklas Höglund , group: 11 (2022)
+// Work package 2
+// Exercise 1
+// Submission code: 
+
+#include <stdio.h> // print,scan and getchar
+#include <string.h> //used in the for loop to iterate through movement commands with strlen
+
+
+
+int main(int argc, char* argv[])
+{
+    unsigned char x = argv[1];
+    printf("%d", x);
+    
+
+    // logic math behind converting bin to hex
+    /* while (binaryval != 0) { // While the binaryval isn't 0
+        remainder = binaryval % 10; // Set the remainder variable to binaryval divided by 10 using the modulus operator
+        hexadecimalval = hexadecimalval + remainder * i; // To the hexadecimalval variable, add itself plus the remainder * i 
+        i = i * 2; // Double the i variable
+        binaryval = binaryval / 10; // Divide binaryval variable by 10
+    } */
+
+}
+
