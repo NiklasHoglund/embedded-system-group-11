c9ea57e3eb01e587ee0a79ba869d75a5ca0c47ac
Ahmed Yasser <70852038+AhmedYasser-gu@users.noreply.github.com>
2022-02-08 23:32:17 +0100

    Update exercise4.cpp
    commenting exe 4

--- WP3/exercise4.cpp
+++ WP3/exercise4.cpp
@@ -1,3 +1,7 @@
+#include <Keypad.h>
+
+
+
 // (C) Ahmed Yasser, Axel Broberg, Niklas Höglund , group: 11 (2022)
 // Work package 3
 // Exercise 4
@@ -5,33 +9,38 @@
 
 #include <Keypad.h>
 
-const byte rows = 4; //four rows
-const byte columns = 4; //four columns
+const byte rows = 4; //our four rows from keypad
+const byte columns = 4; //our four columns from keypad
 
-char keys[rows][columns] = {
-  {'1','2','3','A'},
-  {'4','5','6','B'},
-  {'7','8','9','C'},
-  {'*','0','#','D'}
+// Declaring the constants byte's values by rows & columns
+char keyMap[rows][columns] =
+{
+{'1', '2', '3', 'A'}, 
+{'4', '5', '6', 'B'}, 
+{'7', '8', '9', 'C'},
+{'*', '0', '#', 'D'}
 };
 
-byte rowPins[rows] = {11, 10, 9, 8}; //connect to the row pinouts of the keypad
-byte colPins[columns] = {7, 6, 5, 4}; //connect to the column pinouts of the keypad
+byte rowPins[rows] = {11,10,9,8}; //attaching with row pinouts from the keypad
+byte colPins[columns] = {7,6,5,4}; //attaching with col pinouts from the keypad
 
-//Createobject class of keypad
-Keypad ourKeyPad = Keypad( makeKeymap(keys), rowPins, colPins, rows, columns );
+// Initialize object class of our keypad
+Keypad KP= Keypad(makeKeymap(keyMap), rowPins, colPins, rows, columns);
 
+// Starts the system
 void setup(){
   Serial.begin(9600);
 }
   
 void loop(){
-  char keyP = ourKeyPad.getKey();// Read the key pressed from keypad
+  char keyP = KP.getKey();// Read the key input pressed from keypad
   
-  // Print if key pressed
+  // If statement to print out only the clicked button from the keypad
   if (keyP){
+    delay(50);
     Serial.print("Key Clicked on : ");
     // Display the pressed keys
     Serial.println(keyP);
+    delay(50);
   }
 }
