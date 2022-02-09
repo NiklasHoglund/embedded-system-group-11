4f67c38f8cb3bfd87c912bf90e4d13589785ab7b
Ahmed Yasser <70852038+AhmedYasser-gu@users.noreply.github.com>
2022-02-09 10:43:44 +0100

    Update exercise1.cpp
    commented exe 1

--- WP3/exercise1.cpp
+++ WP3/exercise1.cpp
@@ -5,15 +5,22 @@
 
 void setup()
 {
+  // Initialize pins
 	pinMode(0, OUTPUT);
 	pinMode(1, OUTPUT);
 }
 
 void loop()
 {
+  // Setting on pin 0 and 1 to high = ON  
+  // Note : pin 0 LED only works when Push Button clicked it
 	digitalWrite(0, HIGH);
 	digitalWrite(1, HIGH);
+  // Delay
 	delay(500); // Wait for 500 millisecond(s)
+  // Turns off yellow LED
 	digitalWrite(1, LOW);
+  // Delay
 	delay(500); // Wait for 500 millisecond(s)
+  // Then loops all over again
 }
