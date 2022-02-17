db14a5db5f78cfc5003662a2a654a051b27e6e5e
Ahmed Yasser <70852038+AhmedYasser-gu@users.noreply.github.com>
2022-02-17 13:41:20 +0100

    Update exercise2.cpp
    sub code

--- WP4/exercise2.cpp
+++ WP4/exercise2.cpp
@@ -1,7 +1,7 @@
 // (C) Ahmed Yasser, Axel Broberg, Niklas Höglund , group: 11 (2022)
 // Work package 4
 // Exercise 2
-// Submission code:
+// Submission code: Group 11: DD11DDDD
 
 #include <Servo.h>
 
