4b3ae897d4336fe3a9cd16910e5ee3665cc091df
AxelBroberg <axellakris2@gmail.com>
2022-02-17 13:44:41 +0100

    wp4 done

--- WP4/exercise2.cpp
+++ WP4/exercise2.cpp
@@ -1,7 +1,7 @@
 // (C) Ahmed Yasser, Axel Broberg, Niklas Höglund , group: 11 (2022)
 // Work package 4
 // Exercise 2
-// Submission code: Group 11: DD11DDDD
+// Submission code: DD11DDDD
 
 #include <Servo.h>
 
@@ -9,6 +9,8 @@ int position = 0;
 
 int i = 0;
 bool rise = false;
+int x = 0;
+int seconds = 0;
 
 Servo servo1;
 
@@ -21,63 +23,52 @@ const uint16_t tl_comp = 65535;
 void moveServo();
 
 void setup() {
-    servo1.attach(A0, 500, 2500);
+    servo1.attach(5, 500, 2500);
     Serial.begin(9600);
 
     // Set to output
-    // Port B Data Direction Register
     DDRB |= (1 << led_pin);
-
-    //Reset Timer0 Control Reg A
-    // Timer/Counter Control Register 0A
-    TCCR0A = 0;
+    //Reset Timer2 Control Reg A
+    TCCR2A = 0;
 
     // Set to prescaler of 256
-    // Timer/Counter Control Register 0B
-    TCCR0B |= (1 << CS12);
-    TCCR0B &= ~(1 << CS11);
-    TCCR0B &= ~(1 << CS10);
+    TCCR2B |= (1 << CS22);
+    TCCR2B &= ~(1 << CS21);
+    TCCR2B &= ~(1 << CS20);
 
-    // Reset Timer0 and set compare value
-    // Timer/Counter 0
-    TCNT0 = tl_load;
-    // Timer/Counter 0 Output Compare Register A
-    OCR0A = tl_comp;
+    // Reset Timer2 and set compare value
+    TCNT2 = tl_load;
+    OCR2A = tl_comp;
 
-    // Enable timer0 compare interrupt
-    // Timer/Counter Interrupt Mask Register
-    // Timer/Counter0 Output Compare Match A Interrupt Enable
-    TIMSK0 = (1 << OCIE0A);
+    // Enable timer2 compare interrupt
+    TIMSK2 = (1 << OCIE2A);
 
     // Enable global interrupts
     sei();
 }
 
 void loop() {
+    if (x > 999) {
+        moveServo();
+        x = 0;
+        seconds = seconds + 1;
+        Serial.println(seconds);
+    }
 }
 
 // Create interrupt service routine
-ISR(TIMER0_COMPA_vect) {
-    // Reset timer1 to 0
-    TCNT0 = tl_load;
-
-    moveServo();
+ISR(TIMER2_COMPA_vect) {
+    // Reset timer2 to 0
+    TCNT2 = tl_load;
+    x = x + 1;
 }
 
 void moveServo() {
-    // If timer is at max value, reset to 0
-    if (i == 180) {
+    if (i == 179) {
         i = 0;
     }
 
-    // Increment timer value
-    i++;
-
-    // Set servo value
+    i = i + 3;
     servo1.write(i);
 
-    // If statement to make sure that the same clock value isn't printed three times in a row
-    if (servo1.read() % 3 == 0) {
-        Serial.println(servo1.read() / 3);
-    }
 }
