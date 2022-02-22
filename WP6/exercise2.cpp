5a537cd0c1685d340fde8b753a36fc4439278d97
Ahmed Yasser <70852038+AhmedYasser-gu@users.noreply.github.com>
2022-02-22 20:07:05 +0100

    Update exercise2.cpp
    few changes for first req , smth left

--- WP6/exercise2.cpp
+++ WP6/exercise2.cpp
@@ -4,8 +4,8 @@
 // Submission code: 
 
 // Initializations
-const int pingPin = 7;
-const int ledPin = 9;
+const int signalPin = 7;
+const int negPin = 9;
 
 void setup() {
   pinMode(2, OUTPUT);
@@ -13,53 +13,53 @@ void setup() {
   pinMode(4, OUTPUT);
   pinMode(5, OUTPUT);
   
-  // start connection
+ // Start connection
   Serial.begin(9600);
-  pinMode(ledPin, OUTPUT);
+  pinMode(negPin, OUTPUT);
 }
 
 void loop() {
-  // establish variables for duration of the ping,
-  // and the distance result in inches and centimeters:
+ // establish variables for duration of the ping,
+ // and the distance result in inches and centimeters:
   long duration, cm;
 
-  // The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
-  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
-  pinMode(pingPin, OUTPUT);
-  digitalWrite(pingPin, LOW);
+ // The PING :is triggered by a HIGH pulse of 2 or more microseconds.
+ // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  pinMode(signalPin, OUTPUT);
+  digitalWrite(signalPin, LOW);
   delayMicroseconds(2);
-  digitalWrite(pingPin, HIGH);
+  digitalWrite(signalPin, HIGH);
   delayMicroseconds(5);
-  digitalWrite(pingPin, LOW);
+  digitalWrite(signalPin, LOW);
 
-  // The same pin is used to read the signal from the PING))): a HIGH
-  // pulse whose duration is the time (in microseconds) from the sending
-  // of the ping to the reception of its echo off of an object.
-  pinMode(pingPin, INPUT);
-  duration = pulseIn(pingPin, HIGH);
+ // The same pin is used to read the signal from the PING))): a HIGH
+ // pulse whose duration is the time (in microseconds) from the sending
+ // of the ping to the reception of its echo off of an object.
+  pinMode(signalPin, INPUT);
+  duration = pulseIn(signalPin, HIGH);
 
-  // convert the time into a distance
+  // Convert time into  distance
   cm = microsecondsToCentimeters(duration);
 
   // Print the distance
-  Serial.print("Distance: ");
+  Serial.print("Distance between target : ");
   Serial.print(cm);
   Serial.print("cm");
   Serial.println();
 
   // Turn on the LED if the object is too close:
   if(cm < 100) {
-    digitalWrite(ledPin, HIGH);
+    digitalWrite(negPin, HIGH);
   }
   else {
-    digitalWrite(ledPin, LOW);
+    digitalWrite(negPin, LOW);
   }
   delay(100);
 }
 
 long microsecondsToCentimeters(long microseconds) {
-  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
-  // The ping travels out and back, so to find the distance of the
-  // object we take half of the distance travelled.
+ // The speed of sound is 340 m/s or 29 microseconds per centimeter.
+ // The ping travels out and back, so to find the distance of the
+ // object we take half of the distance travelled.
   return microseconds / 29 / 2;
 }
