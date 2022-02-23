e8f619de96b4d0fc5304c9970b1097e6d64696c9
AxelBroberg <axellakris2@gmail.com>
2022-02-23 14:17:33 +0100

    exercise 2

--- WP6/exercise2.cpp
+++ WP6/exercise2.cpp
@@ -7,59 +7,104 @@
 const int signalPin = 7;
 const int negPin = 9;
 
+#define LED1 5
+#define LED2 4
+#define LED3 3
+#define LED4 2
+
 void setup() {
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  
- // Start connection
-  Serial.begin(9600);
-  pinMode(negPin, OUTPUT);
+    pinMode(2, OUTPUT);
+    pinMode(3, OUTPUT);
+    pinMode(4, OUTPUT);
+    pinMode(5, OUTPUT);
+
+    // Start connection
+    Serial.begin(9600);
+    pinMode(negPin, OUTPUT);
 }
 
 void loop() {
- // establish variables for duration of the ping,
- // and the distance result in inches and centimeters:
-  long duration, cm;
+    // variables for duration of the ping,
+    // and distance result in centimeters:
+    long duration, cm;
 
- // The PING :is triggered by a HIGH pulse of 2 or more microseconds.
- // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
-  pinMode(signalPin, OUTPUT);
-  digitalWrite(signalPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(signalPin, HIGH);
-  delayMicroseconds(5);
-  digitalWrite(signalPin, LOW);
+    // The PING :is triggered by a HIGH pulse of 2 or more microseconds.
+    // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+    pinMode(signalPin, OUTPUT);
+    digitalWrite(signalPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(signalPin, HIGH);
+    delayMicroseconds(5);
+    digitalWrite(signalPin, LOW);
 
- // The same pin is used to read the signal from the PING))): a HIGH
- // pulse whose duration is the time (in microseconds) from the sending
- // of the ping to the reception of its echo off of an object.
-  pinMode(signalPin, INPUT);
-  duration = pulseIn(signalPin, HIGH);
+    // The same pin is used to read the signal from the PING))): a HIGH
+    // pulse whose duration is the time (in microseconds) from the sending
+    // of the ping to the reception of its echo off of an object.
+    pinMode(signalPin, INPUT);
+    duration = pulseIn(signalPin, HIGH);
 
-  // Convert time into  distance
-  cm = microsecondsToCentimeters(duration);
+    // Convert time into  distance
+    cm = microsecondsToCentimeters(duration);
 
-  // Print the distance
-  Serial.print("Distance between target : ");
-  Serial.print(cm);
-  Serial.print("cm");
-  Serial.println();
+    // Print the distance
+    Serial.print("Distance between object : ");
+    Serial.print(cm);
+    Serial.print("cm");
+    Serial.println();
 
-  // Turn on the LED if the object is too close:
-  if(cm < 100) {
-    digitalWrite(negPin, HIGH);
-  }
-  else {
-    digitalWrite(negPin, LOW);
-  }
-  delay(100);
+    // Turn on the LED if the object is too close:
+    if(cm <= 200 && cm > 125) { // 1 LED ON
+        digitalWrite(LED1, HIGH);
+        digitalWrite(LED2, LOW);
+        digitalWrite(LED3, LOW);
+        digitalWrite(LED4, LOW);
+        digitalWrite(negPin, HIGH);
+        tone(negPin, 50);
+    } else if (cm <= 125 && cm > 75) { // 2 LED ON
+        digitalWrite(LED1, HIGH);
+        digitalWrite(LED2, HIGH);
+        digitalWrite(LED3, LOW);
+        digitalWrite(LED4, LOW);
+        digitalWrite(negPin, HIGH);
+        tone(negPin, 75);
+    } else if (cm <= 75 && cm > 50) { // 3 LED ON
+        digitalWrite(LED1, HIGH);
+        digitalWrite(LED2, HIGH);
+        digitalWrite(LED3, HIGH);
+        digitalWrite(LED4, LOW);
+        digitalWrite(negPin, HIGH);
+        tone(negPin, 100);
+    } else if (cm <= 50 && cm > 25) { // 4 LED ON
+        digitalWrite(LED1, HIGH);
+        digitalWrite(LED2, HIGH);
+        digitalWrite(LED3, HIGH);
+        digitalWrite(LED4, HIGH);
+        digitalWrite(negPin, HIGH);
+        tone(negPin, 150);
+    } else if (cm <= 25) {
+        digitalWrite(negPin, HIGH);
+        tone(negPin, 200);
+
+        digitalWrite(LED1, HIGH);
+        digitalWrite(LED2, HIGH);
+        digitalWrite(LED3, HIGH);
+        digitalWrite(LED4, HIGH);
+        delay(1000);
+        digitalWrite(LED1, LOW);
+        digitalWrite(LED2, LOW);
+        digitalWrite(LED3, LOW);
+        digitalWrite(LED4, LOW);
+        delay(1000);
+
+    } else if (cm > 200) {
+        digitalWrite(negPin, LOW); // Sound sleep
+    }
+
+    delay(100);
 }
 
 long microsecondsToCentimeters(long microseconds) {
- // The speed of sound is 340 m/s or 29 microseconds per centimeter.
- // The ping travels out and back, so to find the distance of the
- // object we take half of the distance travelled.
-  return microseconds / 29 / 2;
+// The speed of sound is 340 m/s
+// Logic to find distance of the object ( take half )
+    return microseconds / 29 / 2;
 }
