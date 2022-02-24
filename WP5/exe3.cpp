721c2a8db522412b13107060a0bcf1324ca9be43
Niklas Hoglund <nikhog@net.chalmers.se>
2022-02-24 20:32:18 +0100

    commented

--- WP5/exe3.cpp
+++ WP5/exe3.cpp
@@ -1,9 +1,16 @@
+// (C) Ahmed Yasser, Axel Broberg, Niklas Höglund , group: 11 (2022)
+// Work package 5
+// Exercise 3
+// Submission code: EEE11EEE
+
+//Variables
 int light;
 int lightPercentage;
 int analogTempPin = A0;
+int analogLightPin = A1;
 
 #include <math.h>
-/*Code to convert the sensor reading to degrees celsius was used from example code at
+/*Code to convert the sensor reading to degrees celsius that was used from example code at
 http://arduino-sensor.org/sensor-kits/arduino-ky-013-temperature-sensor-module/ */
 double Thermistor(int RawADC) {
     double Temp;
@@ -14,29 +21,32 @@ double Thermistor(int RawADC) {
 }
 
 void setup() {
-    pinMode(A0, INPUT);
-    pinMode(A1, INPUT);
-    pinMode(2, OUTPUT);
-    pinMode(3, OUTPUT);
-    pinMode(4, OUTPUT);
-    // start system
+    // Initialize pins
+    pinMode(A0, INPUT); //Pin Where the Analog Temperature Sensor is connected to
+    pinMode(A1, INPUT); //Pin Where the Light Dependent Resistor is connected to
+    pinMode(2, OUTPUT); //GREEN
+    pinMode(3, OUTPUT); //RED
+    pinMode(4, OUTPUT); //BLUE
+    // start serial communication
     Serial.begin(9600);
 }
 
 void loop() {
+    //Reading sensor value from analog pin A0
     int valueFromPin = analogRead(analogTempPin);
     double temp = Thermistor(valueFromPin);
-    light = analogRead(A1);
+    //Reading sensor value from analog pin A1
+    light = analogRead(analogLightPin);
 
     Serial.print("Reading of light: ");
     Serial.println(light);
-    lightPercentage = map(light, 0, 1023, 100, 0);
-    Serial.print(lightPercentage);
+    lightPercentage = map(light, 0, 1023, 100, 0);  //Changing the reading value from 0-1023 to percentage 0-100
+    Serial.print(lightPercentage);                  //Showing the light reading in percentage and printing
     Serial.println("%");
-    Serial.println("Temp: ");
+    Serial.println("Temp: ");                       //Printing the temperature converted to degrees Celsius from sensor reading
     Serial.println(temp);
 
-    // If statements to handle the conditions we wanted as requested
+    // If statements to handle the conditions we wanted as stated in the exercise
     if (lightPercentage == 0) {
         if (temp < -12) {
             digitalWrite(4, LOW); // BLUE
