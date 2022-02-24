1308475722392382d2171ccc98efd50455aebbe4
Ahmed Yasser <70852038+AhmedYasser-gu@users.noreply.github.com>
2022-02-24 21:28:32 +0100

    Create exercise3.cpp
    working on exe 3

---
+++ WP6/exercise3.cpp
@@ -0,0 +1,26 @@
+// (C) Ahmed Yasser, Axel Broberg, Niklas Höglund , group: 11 (2022)
+// Work package 6
+// Exercise 3
+// Submission code: 
+
+#include <Adafruit_NeoPixel.h>
+
+#define in 2
+byte R, G, B;
+
+
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(24, in, NEO_GRB + NEO_KHZ800);
+    
+void setup() 
+{
+
+
+}
+
+
+void loop() 
+{
+
+  
+}
